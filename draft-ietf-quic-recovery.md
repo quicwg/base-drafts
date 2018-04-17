8821bee05da7a9e5a63f6cc8197b706917ced326
Martin Thomson <martin.thomson@gmail.com>
2018-04-18 08:26:11 +1000

    Changelog for -11 (#1291)
    * Changelogs for -11 release
    
    * Add a tagging script
    
    * Expand help
    
    * Added more to changelogs
    
    * added to changelogs for -11

--- draft-ietf-quic-recovery.md
+++ draft-ietf-quic-recovery.md
@@ -1087,6 +1087,15 @@ This document has no IANA actions.  Yet.
 > **RFC Editor's Note:**  Please remove this section prior to
 > publication of a final version of this document.
 
+## Since draft-ietf-quic-recovery-10
+
+- Improved text on ack generation (#1139, #1159)
+- Make references to TCP recovery mechanisms informational (#1195)
+- Define time_of_last_sent_handshake_packet (#1171)
+- Added signal from TLS the data it includes needs to be sent in a Retry packet
+  (#1061, #1199)
+- Minimum RTT (min_rtt) is initialized with an infinite value (#1169)
+
 ## Since draft-ietf-quic-recovery-09
 
 No significant changes.
---
+++ tag.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Tag files for submission.
+#
+# You shouldn't need to use this unless you are tagging files for which you are
+# not an author.  Use `git tag -a` instead.
+#
+# This script exists because
+# https://trac.tools.ietf.org/tools/ietfdb/ticket/2390 still isn't fixed.
+
+if [[ $# -eq 0 ]]; then
+    files=(transport tls recovery http)
+else
+    files=("$@")
+fi
+
+enabled() {
+    r="$1"; shift
+    for e; do [[ "$e" == "$r" ]] && return 0; done
+    return 1
+}
+
+tag() {
+    message="Tag for $2 created by $(git config --get user.name)"
+    git tag -c user.email="$1" tag -am "$message" "$2"
+}
+
+declare -A authors=( \
+    [transport]=martin.thomson@gmail.com \
+    [tls]=martin.thomson@gmail.com \
+    [recovery]=ianswett@google.com \
+    [http]=mbishop@evequefou.be \
+    [invariants]=martin.thomson@gmail.com \
+    [qpack]=afrind@fb.com \
+    [spin-exp]=ietf@trammell.ch \
+)
+
+for t in $(make show-next); do
+    r="${t%-[0-9][0-9]}"
+    r="${r#draft-ietf-quic-}"
+    if enabled "$r" "${files[@]}"; then
+        tag "${authors[$r]}" "$t"
+    fi
+done
